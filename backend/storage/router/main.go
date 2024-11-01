45f2337c0b19c5a88839b69f2f678c37773be18a
Evgeny Berg <berg.evg.1@gmail.com>
2024-11-01 13:35:27 +0400

    storage microservice

---
+++ backend/storage/router/main.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	_ "fileStorage/docs"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func New() *gin.Engine {
+	router := gin.Default()
+
+	router.Static("/files", "./static")
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.GET("/api", func(ctx *gin.Context) { ctx.Redirect(http.StatusFound, "/swagger/index.html") })
+
+	router.POST("/upload", upload)
+
+	return router
+}
