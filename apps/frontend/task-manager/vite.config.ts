b52cdc681e859e2d69b10fa7b308e26ef8217f3d
Evgeny Berg <evberg@simple-it.pro>
2025-04-29 23:59:28 +0400

    tmp

--- apps/frontend/task-manager/vite.config.ts
+++ apps/frontend/task-manager/vite.config.ts
@@ -2,51 +2,49 @@
 import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
 import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 import react from "@vitejs/plugin-react";
-// import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig(() => ({
-  root: __dirname,
-  cacheDir: "../../../node_modules/.vite/apps/task-manager",
-  server: {
-    port: 1346,
-    host: "localhost"
-  },
-  preview: {
-    port: 2346,
-    host: "localhost"
-  },
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(["*.md"])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  build: {
-    outDir: "../../../dist/apps/task-manager",
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true
-    }
-  },
-  resolve: {
-    // alias: {
-    //   "shared": resolve(__dirname, "./src/shared"),
-    //   "pages": resolve(__dirname, "./src/pages"),
-    //   "widgets": resolve(__dirname, "./src/widgets"),
-    //   "api": resolve(__dirname, "./src/app/api"),
-    //   "store": resolve(__dirname, "./src/app/store")
-    // }
-  },
-  test: {
-    watch: false,
-    globals: true,
-    environment: "jsdom",
-    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    reporters: ["default"],
-    coverage: {
-      reportsDirectory: "../../../coverage/apps/task-manager",
-      provider: "v8" as const
-    }
-  }
+	root: __dirname,
+	cacheDir: "../../../node_modules/.vite/apps/task-manager",
+	server: {
+		port: 1346
+	},
+	preview: {
+		port: 2346,
+		host: "localhost"
+	},
+	plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(["*.md"])],
+	// Uncomment this if you are using workers.
+	// worker: {
+	//  plugins: [ nxViteTsPaths() ],
+	// },
+	build: {
+		outDir: "../../../dist/apps/task-manager",
+		emptyOutDir: true,
+		reportCompressedSize: true,
+		commonjsOptions: {
+			transformMixedEsModules: true
+		}
+	},
+	resolve: {
+		// alias: {
+		//   "shared": resolve(__dirname, "./src/shared"),
+		//   "pages": resolve(__dirname, "./src/pages"),
+		//   "widgets": resolve(__dirname, "./src/widgets"),
+		//   "api": resolve(__dirname, "./src/app/api"),
+		//   "store": resolve(__dirname, "./src/app/store")
+		// }
+	},
+	test: {
+		watch: false,
+		globals: true,
+		environment: "jsdom",
+		include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		reporters: ["default"],
+		coverage: {
+			reportsDirectory: "../../../coverage/apps/task-manager",
+			provider: "v8" as const
+		}
+	}
 }));
