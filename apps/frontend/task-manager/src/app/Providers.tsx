2f2eb85284f7dcb58c43a3165c7b213330bc40ca
Evgeny Berg <berg.evg.1@gmail.com>
2026-01-10 17:12:42 +0400

    remove i18n

--- apps/frontend/task-manager/src/app/Providers.tsx
+++ apps/frontend/task-manager/src/app/Providers.tsx
@@ -5,9 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { darkThemeConfig, lightThemeConfig } from "@task-manager/ant-config";
 import { App as AntApp, theme as antTheme, FloatButton, ThemeConfig } from "antd";
 import { ThemeProvider } from "antd-style";
-import { I18nextProvider } from "react-i18next";
 
-import i18n from "./i18n";
 import { useAppStore } from "./store/app";
 
 interface ProvidersProps {
@@ -37,15 +35,13 @@ const Providers: React.FC<ProvidersProps> = props => {
 	return (
 		<ThemeProvider theme={themeConfig} themeMode={theme}>
 			<QueryClientProvider client={queryClient}>
-				<I18nextProvider i18n={i18n}>
-					<AntApp className="h-full">
-						{props.children}
-						<FloatButton
-							icon={theme === "light" ? <MoonOutlined /> : <SunOutlined />}
-							onClick={toggleTheme}
-						/>
-					</AntApp>
-				</I18nextProvider>
+				<AntApp className="h-full">
+					{props.children}
+					<FloatButton
+						icon={theme === "light" ? <MoonOutlined /> : <SunOutlined />}
+						onClick={toggleTheme}
+					/>
+				</AntApp>
 			</QueryClientProvider>
 		</ThemeProvider>
 	);
--- apps/frontend/task-manager/src/app/router.tsx
+++ apps/frontend/task-manager/src/app/router.tsx
@@ -7,45 +7,12 @@ import Layout from "../widgets/Layout";
 const Profile = lazySuspense(() => import("../pages/profile"), <FullSizeLoader />);
 const Page = lazySuspense(() => import("../pages/page"), <FullSizeLoader />);
 const CurrentWorkspace = lazySuspense(() => import("../pages/currentWorkspace"), <FullSizeLoader />);
-// const Login = lazySuspense(() => import("../pages/login"), <FullSizeLoader />);
-// const SignUp = lazySuspense(() => import("../pages/signUp"), <FullSizeLoader />);
-// const ResetPassword = lazySuspense(() => import("../pages/resetPassword"), <FullSizeLoader />);
-// const ConfirmEmail = lazySuspense(() => import("../pages/confirmEmail"), <FullSizeLoader />);
-// const NewPassword = lazySuspense(() => import("../pages/newPassword"), <FullSizeLoader />);
-// const AuthLayout = lazySuspense(() => import("../widgets/AuthLayout"), <FullSizeLoader />);
 
 export default createBrowserRouter([
 	{
 		path: "",
 		element: <Navigate to="/profile" replace />
 	},
-
-	// {
-	// 	path: "/",
-	// 	Component: AuthLayout,
-	// 	children: [
-	// 		{
-	// 			path: "/login",
-	// 			Component: Login
-	// 		},
-	// 		{
-	// 			path: "/sign-up",
-	// 			Component: SignUp
-	// 		},
-	// 		{
-	// 			path: "/reset-password",
-	// 			Component: ResetPassword
-	// 		},
-	// 		{
-	// 			path: "/confirm-email",
-	// 			Component: ConfirmEmail
-	// 		},
-	// 		{
-	// 			path: "/new-password",
-	// 			Component: NewPassword
-	// 		}
-	// 	]
-	// },
 
 	{
 		path: "/",
